f475d252c2f3baf24ff44e2f49da55011a842b4e
adamstallard <205792+adamstallard@users.noreply.github.com>
2024-11-11 02:08:58 -0800

    fix local import dirs

--- contracts/Solution.sol
+++ contracts/Solution.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import {ICrowdFund} from "interfaces/ICrowdFund.sol";
+import {ICrowdFund} from "./interfaces/ICrowdFund.sol";
 
 struct Cycle {
     uint256 number;
--- contracts/Updraft.sol
+++ contracts/Updraft.sol
@@ -4,9 +4,9 @@ pragma solidity ^0.8.28;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import {ICrowdFund} from "interfaces/ICrowdFund.sol";
-import {Idea} from "Idea.sol";
-import {Solution} from "Solution.sol";
+import {ICrowdFund} from "./interfaces/ICrowdFund.sol";
+import {Idea} from "./Idea.sol";
+import {Solution} from "./Solution.sol";
 
 contract Updraft is Ownable, ICrowdFund {
     using SafeERC20 for IERC20;
