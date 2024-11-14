cf23d4af369f0f13b769918fd7b26be50df3dcfc
adamstallard <205792+adamstallard@users.noreply.github.com>
2024-11-14 14:56:04 -0800

    verify deployed contracts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,6 +2,8 @@ import type { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
 import "@truffle/dashboard-hardhat-plugin";
 
+import { etherscan as etherscanKey } from "./apiKeys.json";
+
 const config: HardhatUserConfig = {
   solidity: {
     version: "0.8.27",
@@ -11,6 +13,12 @@ const config: HardhatUserConfig = {
         runs: 10
       }
     }
+  },
+  etherscan: {
+    apiKey: etherscanKey
+  },
+  sourcify: {
+    enabled: true
   },
   defaultNetwork: "truffleDashboard",
 };
