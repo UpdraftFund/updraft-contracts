7e1cbaee15de0f77373bfd1f5de3c5b57c7c39b7
adamstallard <205792+adamstallard@users.noreply.github.com>
2024-11-17 16:27:48 -0800

    test that stake is allocated properly when Solution is created

--- test/updraft.ts
+++ test/updraft.ts
@@ -2,20 +2,25 @@ import hre from 'hardhat';
 import { parseUnits, toHex } from 'viem';
 import { assert, expect } from 'chai';
 import { loadFixture } from '@nomicfoundation/hardhat-toolbox-viem/network-helpers';
-import { getEventsFromTx, walletAddress } from './utilities/helpers.ts'
+import { getEventsFromTx, walletAddress } from './utilities/helpers.ts';
 
 const profileData = {
   "team": "Adam, Victor, Bastin, Beigi, Amirerfan",
   "about": "Creators of Updraft"
-}
+};
 
 const ideaData = {
   "name": "Build a monkey cage.",
   "description": "We need a cage for our monkey."
-}
+};
+
+const solutionData = {
+  "name": "Build a monkey cage.",
+  "description": "We will build a cage for your monkey."
+};
 
 const antiSpamFee = parseUnits('1', 18); // 1 UPD
-const contribution = parseUnits('10', 18) // 10 UPD
+const contribution = parseUnits('10', 18); // 10 UPD
 
 const deployUpdraft = async () => {
   const upd = await hre.viem.deployContract('UPDToken');
@@ -30,21 +35,21 @@ const deployUpdraft = async () => {
   return { updraft, upd };
 };
 
-const approveUpdraftToSpendUPD = async () => {
+const deployUpdraftAndApproveToSpendUPD = async () => {
   const { updraft, upd } = await loadFixture(deployUpdraft);
   await upd.write.approve([updraft.address, parseUnits('100000000000', 18)]);
   return { updraft, upd };
-}
+};
 
 const deployIdeaAndGetTx = async () => {
-  const { updraft } = await loadFixture(approveUpdraftToSpendUPD);
+  const { updraft } = await loadFixture(deployUpdraftAndApproveToSpendUPD);
   const publicClient = await hre.viem.getPublicClient();
   //  100% is 1000000 (percentScale is 1000000 in Updraft.sol)
   const contributorFee = 10000; // 1%
   const hash = await updraft.write.createIdea([contributorFee, contribution, toHex(ideaData)]);
   const transaction = await publicClient.getTransactionReceipt({hash});
   return { transaction };
-}
+};
 
 const deployIdeaAndGetContract = async () => {
   const { transaction } = await loadFixture(deployIdeaAndGetTx);
@@ -52,17 +57,44 @@ const deployIdeaAndGetContract = async () => {
   const { idea } = events.find(event => event.eventName === 'IdeaCreated').args;
   const contract = await hre.viem.getContractAt('Idea',idea);
   return { contract };
-}
+};
+
+const deploySolutionAndGetTx = async () => {
+  const { updraft, upd } = await loadFixture(deployUpdraftAndApproveToSpendUPD);
+  const { contract: idea } = await loadFixture(deployIdeaAndGetContract);
+  const publicClient = await hre.viem.getPublicClient();
+  const hash = await updraft.write.createSolution(
+    [
+      idea.address,
+      upd.address,
+      parseUnits('100', 18),
+      parseUnits('10000', 18),
+      3940876877,
+      10000,
+      toHex(solutionData)
+    ]
+  );
+  const transaction = await publicClient.getTransactionReceipt({hash});
+  return { transaction };
+};
+
+const deploySolutionAndGetContract = async () => {
+  const { transaction } = await loadFixture(deploySolutionAndGetTx);
+  const events = await getEventsFromTx('Updraft', transaction);
+  const { solution } = events.find(event => event.eventName === 'SolutionCreated').args;
+  const contract = await hre.viem.getContractAt('Solution',solution);
+  return { contract };
+};
 
 describe('Updraft', () => {
   it('should deploy', async () => {
     await loadFixture(deployUpdraft);
   });
   it('should be approved to spend UPD', async () => {
-    await loadFixture(approveUpdraftToSpendUPD);
+    await loadFixture(deployUpdraftAndApproveToSpendUPD);
   });
   it('should create a profile', async () => {
-    const { updraft } = await loadFixture(approveUpdraftToSpendUPD);
+    const { updraft } = await loadFixture(deployUpdraftAndApproveToSpendUPD);
     await updraft.write.updateProfile([toHex(profileData)]);
   });
   describe('Creating an Idea', () => {
@@ -80,6 +112,13 @@ describe('Updraft', () => {
       const { contract: idea } = await loadFixture(deployIdeaAndGetContract);
       const[tokens, shares] = await idea.read.checkPosition([await walletAddress()]);
       expect(tokens).to.equal(contribution - antiSpamFee);
+    });
+    describe('Creating a Solution to the Idea with a positive stake', () => {
+      it('should result in a positive stake for the caller', async () => {
+        const { contract: solution } = await loadFixture(deploySolutionAndGetContract);
+        const stake = Number(await solution.read.stakes([await walletAddress()]));
+        expect(stake).to.be.above(0);
+      });
     });
   });
 });
